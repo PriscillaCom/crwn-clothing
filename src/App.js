2841563818fcb7039eaf0b7a633c839208af64a4
Priscilla <priscillamoc97@gmail.com>
2021-02-18 17:46:21 -0800

    update our menu-item to nav with Router HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage';
 
 function App() {
 
+  const HatsPage = () => (
+    <div>
+      <h1> Hats Page</h1>
+    </div>
+  );
+
   return (
     <div>
-      <HomePage />
+      {/* Switch gives user more control of what to render, find ONE 
+      Path that matches and it'll load it, won't load multiple paths */}
+      <Switch> 
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   )
 }
