dd409c2027bd1c36a2e6114261d4bcd8b73e3534
Priscilla <priscillamoc97@gmail.com>
2021-03-04 17:12:52 -0800

    implemented firebase utilities, storing authenticated users to firebase

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ShopPage from './pages/shop/shop.jsx';
 import Header from './components/header/header.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx'
 
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 
 import './App.css';
 
@@ -23,12 +23,25 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    //Grab current user's info from auth and storing into db
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot,
+              ...snapShot.data()
+            }
+          });
+        });
+      }
+     else {
+       this.setState({ currentUser: userAuth });
+     }
     });
   }
-
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,40 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyAUoiDsQx0xOkZRMB0XlvB4hF51hkAC8q8",
-    authDomain: "clothing-database-89627.firebaseapp.com",
-    projectId: "clothing-database-89627",
-    storageBucket: "clothing-database-89627.appspot.com",
-    messagingSenderId: "207846376857",
-    appId: "1:207846376857:web:4b17f4a39f0243c63abf08",
-    measurementId: "G-2Y2RHLE6Q7"
+    apiKey: "AIzaSyAiq2PaWn5_WyZlw4BTcjDRzL6X2uv3Adw",
+    authDomain: "clothingstore-database.firebaseapp.com",
+    projectId: "clothingstore-database",
+    storageBucket: "clothingstore-database.appspot.com",
+    messagingSenderId: "966456711483",
+    appId: "1:966456711483:web:53f532fe73806afba47f1e",
+    measurementId: "G-PNYS3J391Q"
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const { displayName, email }= userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName, 
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user',error.message);
+        }
+    }
+
+    return userRef;
+}
 
 firebase.initializeApp(config);
 
